92609912dd17e702964e633293734b7e0543f4f1
Jacques <jacqueskibamba@gmail.com>
2023-12-21 14:35:07 +0300

    added map displayer

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import numpy as np
+
+from map_displayer import display_map
 from measure_worker import brand_matrix, color_matrix, get_brand_distance, get_motorcycle_distance, \
     calculate_measure_using_euclidean_distance, calculate_measure_using_cosine_distance
 from reader import get_dataset_array
 from util import convert_value, get_motorcycle_type, check_matrix, create_measure_distance_dictionary, \
     create_measure_distance_matrix
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
 
 
 def main():
-    measure_distance_dictionary = create_measure_distance_dictionary(get_dataset_array(),
-                                                                     calculate_measure_using_euclidean_distance)
-    data = create_measure_distance_matrix(measure_distance_dictionary)
-    sns.heatmap(data)
-    plt.show()
+    dataset_array = get_dataset_array()
+    display_map(dataset_array, 'Euclidean distance', calculate_measure_using_euclidean_distance)
+    display_map(dataset_array, 'Cosine distance', calculate_measure_using_cosine_distance)
 
 
 if __name__ == '__main__':
---
+++ map_displayer.py
@@ -0,0 +1,12 @@
+from datasetitem import DatasetItem
+from util import create_measure_distance_dictionary, create_measure_distance_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def display_map(dataset_array: [DatasetItem], title, calculate_measure_func):
+    measure_distance_dictionary = create_measure_distance_dictionary(dataset_array,calculate_measure_func)
+    data = create_measure_distance_matrix(measure_distance_dictionary)
+    sns.heatmap(data)
+    plt.title(title)
+    plt.show()
