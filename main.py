95a579b7c7aafaabb4a0be1327ad4efc23c6aac7
Jacques <jacqueskibamba@gmail.com>
2023-12-27 13:47:04 +0300

    introduction to nltk library

--- main.py
+++ main.py
@@ -9,23 +9,14 @@ from datasetitem import DatasetItem
 from map_displayer import display_map
 from measure_worker import brand_matrix, color_matrix, get_brand_distance, get_motorcycle_distance, \
     calculate_measure_using_euclidean_distance, calculate_measure_using_cosine_distance
+from nlp_demo import test
 from reader import get_dataset_array
 from util import convert_value, get_motorcycle_type, check_matrix, create_measure_distance_dictionary, \
     create_measure_distance_matrix, get_item_by_name
 
 
 def main():
-    likes = []
-    dislikes = []
-
-    recommendation_system = RecommendationSystem()
-    recommendation_system.set_closeness_strategy(ClosenessStrategy.NearDistinctSeeds)
-    recommendation_system.calculate_measure_func(calculate_measure_using_cosine_distance)
-
-    while True:
-        rc = main_loop(recommendation_system, likes, dislikes)
-        if rc == 1:
-            return 0
+    test()
 
 
 if __name__ == '__main__':
---
+++ nlp_demo.py
@@ -0,0 +1,28 @@
+from nltk import pos_tag
+from nltk.tokenize import sent_tokenize, word_tokenize
+import nltk
+
+# nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('averaged_perceptron_tagger')
+from nltk.corpus import stopwords
+
+nltk.download('russian')
+
+
+def test():
+    text = "Я хочу купить большой красивый дом"
+
+    words = word_tokenize(text)
+
+    print(pos_tag(words))
+    """
+    text = "Text to tokenize. And an other"
+    text_2 = word_tokenize("Sir, I protest. I am not a marry man!")
+    english_stop_words = set(stopwords.words("english"))
+    filtered_list = []
+    for word in text_2:
+        if word.casefold() not in english_stop_words:
+            filtered_list.append(word)
+    print(filtered_list)
+    """
