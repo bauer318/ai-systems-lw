e2f80ac3837f9a91310c597962157d983d97f9bf
jacques <jacqueskibamba@gmail.com>
2023-12-29 02:01:23 +0300

    optimized import

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-import numpy as np
-from InquirerPy import inquirer
-from numpy import linalg
-
 from cli import main_loop
 from contentBasedRecomendationSystem.closeness_strategy import ClosenessStrategy
 from contentBasedRecomendationSystem.recomendattion_system import RecommendationSystem
-from datasetitem import DatasetItem
-from map_displayer import display_map
-from measure_worker import brand_matrix, color_matrix, get_brand_distance, get_motorcycle_distance, \
-    calculate_measure_using_euclidean_distance, calculate_measure_using_cosine_distance
-from reader import get_dataset_array
-from util import convert_value, get_motorcycle_type, check_matrix, create_measure_distance_dictionary, \
-    create_measure_distance_matrix, get_item_by_name
+from measure_worker import calculate_measure_using_cosine_distance
 
 
 def main():
